d6df15e63137d69f4b806139234c5abf1a2b5d0a
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-06-15 21:01:20 +0300

    update all file

--- 5-local_variables.ps1
+++ 5-local_variables.ps1
@@ -1,11 +1,2 @@
-# 5-local_variables.ps1
-# Lists environment variables, local variables, and functions in Bash-like format
-
-# Environment variables
-Get-ChildItem Env: | ForEach-Object { "$($_.Name)=$($_.Value)" }
-
-# Local (non-readonly) variables
-Get-Variable | Where-Object { -not ($_.Options -match 'ReadOnly|Constant|AllScope|Private') } | ForEach-Object { "$($_.Name)=$($_.Value)" }
-
-# Functions
-Get-Command -CommandType Function | ForEach-Object { $_.Name }
+#!/bin/bash
+set
--- 6-create_local_variable.ps1
+++ 6-create_local_variable.ps1
@@ -1,4 +1,2 @@
-# 6-create_local_variable.ps1
-# This script creates a local variable named BEST with value "School"
-
-$BEST = "School"
+#!/bin/bash
+BEST="School"

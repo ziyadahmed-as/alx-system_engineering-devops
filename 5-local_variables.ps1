4b69abfab1a63b5e32c642f3fed65821abda56b4
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-06-15 20:38:06 +0300

    update local variable

--- 5-local_variables.ps1
+++ 5-local_variables.ps1
@@ -1,11 +1,11 @@
 # 5-local_variables.ps1
-# Lists environment variables, local variables, and functions
+# Lists environment variables, local variables, and functions in Bash-like format
 
-# List all environment variables in NAME=VALUE format
+# Environment variables
 Get-ChildItem Env: | ForEach-Object { "$($_.Name)=$($_.Value)" }
 
-# List all local variables excluding automatic or readonly variables, in NAME=VALUE format
+# Local (non-readonly) variables
 Get-Variable | Where-Object { -not ($_.Options -match 'ReadOnly|Constant|AllScope|Private') } | ForEach-Object { "$($_.Name)=$($_.Value)" }
 
-# List all functions by name
+# Functions
 Get-Command -CommandType Function | ForEach-Object { $_.Name }
